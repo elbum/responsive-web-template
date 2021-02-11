7f6aa86962d8bff88396cdca8e3fa28cd5aa43ef
elbum <elbumlee@gmail.com>
2021-02-11 12:09:46 +0900

    slide evt

--- js/main.js
+++ js/main.js
@@ -25,7 +25,42 @@ backtotop.addEventListener('click',moveBackToTop);
 // ----------------------------------------------------------------
 // 좌우 슬라이딩 버튼 핸들링
 
-const transformPrev = () => {
+const transformPrev = (event) => {
+    const slidePrev = event.target;
+    const slideNext = slidePrev.nextElementSibling;
+
+    // ul 태그 선택
+    const classList = slidePrev.parentElement.parentElement.nextElementSibling;
+    let activeLi = classList.getAttribute('data-position');
+    const liList = classList.getElementsByTagName('li')
+   
+    /* classList.clientWidth 는 ul 태그의 실질적인 너비
+    liList.length * 260 에서 260 은 각 li 요소의 실질 너비(margin 포함)
+    activeLi 는 data-position 에 있는 현재 위치 
+    즉, liList.length * 260 + Number(activeLi) 는 현재 위치부터 오른쪽으로 나열되야 하는 나머지 카드들의 너비
+    */
+
+    /* classList.clientWidth < (liList.length * 260 + Number(activeLi)) 의미는
+    오른쪽으로 나열될 카드들이 넘친 상태이므로, 왼쪽으로 이동이 가능함
+    */
+   if (classList.clientWidth < (liList.length * 260 + Number(activeLi)))
+   {
+       activeLi = Number(activeLi) - 260 ;
+
+       if(classList.clientWidth> (liList.length * 260 + Number(activaLi))){
+           slidePrev.style.color='#cfd8dc';
+           slidePrev.classList.remove('slide-prev-hover');
+       }
+       
+       slideNext.style.color = '#2f3059';
+       slideNext.classList.add('slide-next-hover');
+
+       classList.style.transition = 'transform 1s';
+       classList.style.transform = 'translateX(' + String(activeLi) + 'px)';
+       classList.setAttribute('data-position',activeLi);
+
+   }
+
     
 }
 const slidePrevList = document.getElementsByClassName('slide-prev');
